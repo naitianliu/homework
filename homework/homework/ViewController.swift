34aa6b5ab6916ccee17cf7f287d990e55f5abd24
naitianliu <naitianliu@gmail.com>
2016-06-04 14:57:06 -0700

    initial commit

---
+++ homework/homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework
+//
+//  Created by Liu, Naitian on 6/4/16.
+//  Copyright © 2016 naitianliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
